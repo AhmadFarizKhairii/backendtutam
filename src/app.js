fbad4f7b5c009085a4df73edb12edfff73bbf490
Ahmad Fariz Khairi <163805090+AhmadFarizKhairii@users.noreply.github.com>
2025-05-04 21:17:23 +0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -15,36 +15,36 @@ app.use(morgan('dev')); // Logging request ke console
 
 // Validasi PG_CONNECTION_STRING
 if (!process.env.PG_CONNECTION_STRING) {
-  console.error('Error: PG_CONNECTION_STRING is not defined in the environment variables.');
-  process.exit(1);
+    console.error('Error: PG_CONNECTION_STRING is not defined in the environment variables.');
+    process.exit(1);
 }
 
 // Inisialisasi Koneksi Database
 (async () => {
-  try {
-    await connectDB(); // Memastikan koneksi database berhasil
-    console.log('Database connection established successfully');
-  } catch (err) {
-    console.error('Failed to connect to the database:', err.message);
-    process.exit(1); // Keluar jika koneksi gagal
-  }
+    try {
+        await connectDB(); // Memastikan koneksi database berhasil
+        console.log('Database connection established successfully');
+    } catch (err) {
+        console.error('Failed to connect to the database:', err.message);
+        process.exit(1); // Keluar jika koneksi gagal
+    }
 })();
 
 // Routes
 app.use('/api', bonRoutes);
 
 // Middleware untuk menangani route yang tidak ditemukan
-app.use((req, res, next) => {
-  res.status(404).json({ error: 'Route not found' });
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found' });
 });
 
 // Middleware untuk menangani error
 app.use((err, req, res, next) => {
-  console.error('Error:', err.stack);
-  res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error:', err.message);
+    res.status(500).json({ error: 'Internal Server Error' });
 });
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
